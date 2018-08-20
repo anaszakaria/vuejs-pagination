0e004039a07583f6d97606d9488a086df63c31af
Anas Zakaria <anaszcm@gmail.com>
2018-08-20 12:21:53 +0800

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+    max-width: 800px;
+    th, td {
+        text-align: left;
+        padding: 8px;
     }
-  }
+    th {
+        background-color: #4CAF50;
+        color: white;
+        cursor: pointer;
+        &:hover {
+            background: #333;
+            color: #FFF;
+        }
+    }
+    tr:nth-child(even){background-color: #f2f2f2}
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,205 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="home" style="overflow-x:auto;">
+        <table>
+            <thead>
+                <tr>
+                    <th @click="sort('name')">Name</th>
+                    <th @click="sort('age')">Age</th>
+                    <th @click="sort('job')">Job</th>
+                    <th @click="sort('gender')">Gender</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in sortedPersons">
+                    <td>{{person.name}}</td>
+                    <td>{{person.age}}</td>
+                    <td>{{person.job}}</td>
+                    <td>{{person.gender}}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <section class="pagination">
+            <ul>
+                <li @click="firstPage"><<</li>
+                <li @click="prevPage"><</li>
+                <li v-for="(index, pageNum) in pageLists" @click="gotoPage(index)">{{ index }}</li>
+                <li @click="nextPage">></li>
+                <li @click="lastPage">>></li>
+            </ul>
+        </section>
+
+        <div class="">
+            <span>debug: sort={{ currentSort }}, dir={{ currentSortDir }}, page={{ currentPage }}, pageSize={{ pageSize }}</span>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    data() {
+        return {
+            persons: [
+                {
+                    name: 'Anas',
+                    age: 38,
+                    job: 'Programmer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Abu',
+                    age: 33,
+                    job: 'Lecturer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Fatimah',
+                    age: 31,
+                    job: 'Lecturer',
+                    gender: 'F'
+                },
+                {
+                    name: 'Zainab',
+                    age: 25,
+                    job: 'Programmer',
+                    gender: 'F'
+                },
+                {
+                    name: 'Hamzah',
+                    age: 42,
+                    job: 'Doctor',
+                    gender: 'M'
+                },
+                {
+                    name: 'Halimah',
+                    age: 34,
+                    job: 'Doctor',
+                    gender: 'F'
+                },
+                {
+                    name: 'Zaki',
+                    age: 45,
+                    job: 'Engineer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Zul',
+                    age: 29,
+                    job: 'Engineer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Farah',
+                    age: 44,
+                    job: 'Engineer',
+                    gender: 'F'
+                },
+                {
+                    name: 'Johan',
+                    age: 38,
+                    job: 'Programmer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Faizul',
+                    age: 39,
+                    job: 'Lecturer',
+                    gender: 'M'
+                },
+                {
+                    name: 'Rohani',
+                    age: 35,
+                    job: 'Programmer',
+                    gender: 'F'
+                },
+            ],
+            currentSort: 'name',
+            currentSortDir: 'asc',
+            pageSize: 4,
+            currentPage: 1
+        }
+    },
+    methods: {
+        sort(s) {
+            //if s == current sort, reverse
+            if(s === this.currentSort) {
+                this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc'
+            }
+            this.currentSort = s
+        },
+        nextPage() {
+            if((this.currentPage*this.pageSize) < this.persons.length) this.currentPage++
+        },
+        prevPage() {
+            if(this.currentPage > 1) this.currentPage--
+        },
+        firstPage() {
+            this.currentPage = 1
+        },
+        lastPage() {
+            this.currentPage = Math.ceil(this.persons.length / this.pageSize)
+        },
+        gotoPage(pageNum) {
+            this.currentPage = pageNum
+        }
+    },
+    computed: {
+        sortedPersons:function() {
+            return this.persons.sort((a,b) => {
+                let modifier = 1
+                if(this.currentSortDir === 'desc') modifier = -1
+                if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier
+                if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier
+                return 0
+            }).filter((row, index) => {
+                let start = (this.currentPage-1)*this.pageSize
+                let end = this.currentPage*this.pageSize
+                if(index >= start && index < end) return true
+            })
+        },
+        pageLists() {
+            const pageLists = []
+            const totalPage = Math.ceil(this.persons.length / this.pageSize)
+            for(let i=0; i<totalPage; i++) {
+                pageLists.push(i + 1)
+            }
+            console.log(pageLists)
+            return pageLists
+        }
+    },
+    created() {
+
+    }
 }
 </script>
+
+<style lang="scss">
+#home > table {
+    margin: 0 auto;
+}
+
+.pagination {
+    max-width: 800px;
+    overflow: auto;
+    margin: 8px auto;
+    ul {
+        overflow: auto;
+        background: #fff2e6;
+        padding: 12px;
+        margin: 8px 0;
+    }
+    li {
+        cursor: pointer;
+        list-style-type: none;
+        float: left;
+        width: 30px;
+        padding: 4px;
+        border-right: 1px solid #CCC;
+        &:hover {
+            background: #333;
+            color: #FFF;
+        }
+    }
+}
+</style>
